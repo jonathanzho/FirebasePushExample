f61e6227454f2b769efb3f3d1b615606bacac081
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2019-02-13 13:06:19 -0800

    From google-services 4.1.0 to 4.0.0. Now the app starts working without crash.

--- app/src/main/java/com/example/jonathan/firebasepushexample/MainActivity.java
+++ app/src/main/java/com/example/jonathan/firebasepushexample/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.jonathan.firebasepushexample;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = " FPE MainActivity";
@@ -15,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    FirebaseApp.initializeApp(getApplicationContext());
+
+    FirebaseInstanceId.getInstance().getInstanceId()
+        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+          @Override
+          public void onComplete(@NonNull Task<InstanceIdResult> task) {
+            if (!task.isSuccessful()) {
+              Log.w(TAG, "getInstanceId failed", task.getException());
+              return;
+            }
+
+            // Get new Instance ID token
+            String token = task.getResult().getToken();
+
+            // Log and toast
+            //String msg = getString(R.string.msg_token_fmt, token);
+            String msg = token;
+            Log.v(TAG, "msg=[" + msg + "]");
+            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+          }
+        });
 
     Log.v(TAG, "onCreate: end");
   }
--- app/src/main/java/com/example/jonathan/firebasepushexample/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/jonathan/firebasepushexample/MyFirebaseMessagingService.java
@@ -18,6 +18,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
   }
 
   @Override
+  public void onNewToken(String s) {
+    Log.d(TAG, "onNewToken: s=[" + s + "]");
+
+    super.onNewToken(s);
+  }
+
+  @Override
   public void onMessageReceived(RemoteMessage remoteMessage) {
     Log.d(TAG, "onMessageReceived");
 
